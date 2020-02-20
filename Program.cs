fc1f47da64d79039276ba5ae948402f07cbfd59a
rudek <rudek@DESKTOP-OOLQ00I>
2020-02-19 19:27:24 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace IterativeStatement_JacobRudek
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter an integer value between 2 and 10");
+
+
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            if (y % 2 == 1)
+            {
+
+                for (int i = 0; i < y - 1; i++)
+
+                Console.WriteLine("You entered an odd number");
+
+            }
+            else
+            {
+
+                for (int i = 0; i < y + 1; i++)
+
+                Console.WriteLine("You entered an even number");
+
+            }
+
+        }
+
+
+
+    }
+            }
+
+
+
+
+
+           
+        
+    
+
